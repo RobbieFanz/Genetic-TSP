f242e11b0045283a8c9f1637249f5ebfbb03a016
RobbieFanz <fanzuttir@gmail.com>
2024-04-18 17:44:38 -0400

    Initial commit

---
+++ TSPGenetic/src/Route.java
@@ -0,0 +1,23 @@
+
+public class Route {
+	private int[][] directions = new int[4][4];
+
+	public Route(int[][] directions) {
+	this.directions = directions;
+	}
+	
+	public int getTime(int[][] map) {
+		int time=0;
+		int pos=0;
+		int[] visited = new int[4];
+		
+		for(int i = 0; i<4; i++) {
+			if()
+		}
+		
+		time += map[pos][0];
+		
+		return time;
+	}
+	
+}
---
+++ TSPGenetic/src/TSPGenetic.java
@@ -0,0 +1,18 @@
+
+public class TSPGenetic {
+	
+	private final int map[][] = new int [][] {{0,3,3,8,9},
+											  {3,0,2,6,5},
+											  {3,2,0,9,7},
+											  {8,6,9,0,2},
+											  {9,5,7,2,0}};
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
